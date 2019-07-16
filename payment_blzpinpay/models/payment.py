bd9225f65d53d73d79e304ee78d363adbb03e034
Ronald Villanueva <ronaldv@blaze.online>
2019-07-16 12:37:06 +0800

    card display info debug

--- payment_blzpinpay/models/payment.py
+++ payment_blzpinpay/models/payment.py
@@ -330,10 +330,9 @@ class PaymentTokenBlzPinpay(models.Model):
             _logger.error('payment.token.blzpinpay_create_customer: Customer error:\n%s', pprint.pformat(customer['error']))
             raise Exception(customer['error']['message'])
 
-        _logger.info('rtv: card_obj: %s', pprint.pformat(customer['response']['card']['customer_token']))  # debug  
         values = {
             'acquirer_ref': customer['response']['card']['customer_token'],
-            'name': '%s - %s' % (token['card']['display_number'], customer_params["description"])
+            'name': '%s - %s' % (customer['response']['card']['display_number'].replace("-",""), customer_params["description"])
         }
         _logger.info('rtv: return values [%s]', pprint.pformat(values))
 
