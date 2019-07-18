028626f1d3bc5961d4e3befcd4095783ced48f99
Ronald Villanueva <ronaldv@blaze.online>
2019-07-18 19:06:43 +0800

    response processing

--- payment_blzpinpay/models/payment.py
+++ payment_blzpinpay/models/payment.py
@@ -144,7 +144,12 @@ class PaymentTransactionBlzPinpay(models.Model):
                           params=charge_params)
         res = r.json()
         _logger.info('_create_blzpinpay_charge: Values received:\n%s', pprint.pformat(res))
-        return res
+        
+        if res.get('error'):
+            _logger.error('payment.token._create_blzpinpay_charge: Customer error:\n%s', pprint.pformat(res['error']))
+            raise Exception(res['error']['message'])
+        
+        return res.get('response')
 
     @api.multi
     def blzpinpay_s2s_do_transaction(self, **kwargs):
@@ -289,7 +294,7 @@ class PaymentTokenBlzPinpay(models.Model):
         # pop credit card info to info sent to create
         for field_name in ["cc_number", "cvc", "cc_holder_name", "cc_expiry", "cc_brand", "blzpinpay_token"]:
             values.pop(field_name, None)
-        return res.get('response')
+        return res
 
 
     def _blzpinpay_create_customer(self, token, description=None, acquirer_id=None):
@@ -326,7 +331,7 @@ class PaymentTokenBlzPinpay(models.Model):
                         auth=(api_key, ''),
                         params=customer_params)
         customer = r.json()
-        _logger.info('rtv: customer_object: %s', pprint.pformat(customer.get('response')))  # debug  
+        _logger.info('rtv: customer_object: %s', pprint.pformat(customer))  # debug  
 
         if customer.get('error'):
             _logger.error('payment.token.blzpinpay_create_customer: Customer error:\n%s', pprint.pformat(customer['error']))
